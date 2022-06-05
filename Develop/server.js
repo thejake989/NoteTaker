527ce0dc2fd06d79d93c029fb6619944720c3516
Jake <thejakethree989@gmail.com>
2022-06-05 01:11:03 -0500

    server, readme update

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+
+// Express
+const app = express();
+
+// Active port selection
+const PORT = process.env.PORT || 3001;
+
+// Data parsing through Express
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// Serving static files in Express, so it will load the files that are in the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// App listener
+app.listen(PORT, function () {
+  console.log("App listening on PORT: http://127.0.0.1:" + PORT);
+});
